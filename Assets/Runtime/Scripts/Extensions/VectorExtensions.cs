a5acdf73dda55a50f858993fcea471bdefabbfbf
weiqiuxiang <weiqiuxiang2010@yahoo.co.jp>
2024-02-14 11:39:53 +0900

    VectorExtensionsを実装＋ReferenceViewer導入

---
+++ Assets/Runtime/Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 SetX(this Vector3 vector, float value)
+        {
+            vector.x = value;
+            return vector;
+        }
+        
+        public static Vector3 SetY(this Vector3 vector, float value)
+        {
+            vector.y = value;
+            return vector;
+        }
+        
+        public static Vector3 SetZ(this Vector3 vector, float value)
+        {
+            vector.z = value;
+            return vector;
+        }
+        
+        public static Vector2 SetX(this Vector2 vector, float value)
+        {
+            vector.x = value;
+            return vector;
+        }
+        
+        public static Vector2 SetY(this Vector2 vector, float value)
+        {
+            vector.y = value;
+            return vector;
+        }
+    }
+}
