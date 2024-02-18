480e99d90ea63d6cd026e688134714050416cbc2
weiqiuxiang <weiqiuxiang2010@yahoo.co.jp>
2024-02-18 21:34:02 +0900

    シーン遷移不具合を修正

--- Assets/Runtime/Script/System/SceneTransitionController.cs
+++ Assets/Runtime/Script/System/SceneTransitionController.cs
@@ -16,6 +16,7 @@ namespace Project.System
     {
         private readonly List<SceneDefine> history = new List<SceneDefine>();
         public SceneDefine CurrentSceneDefine { get; private set; }
+        private Scene currentScene;
         private SceneBase currentSceneBase;
         private readonly IAssetsLoader loader;
         public bool HasBackScene => history.Count > 1;  //戻れるシーンはあるか？
@@ -25,10 +26,13 @@ namespace Project.System
         {
             this.loader = loader;
 
+            currentScene = startScene;
             currentSceneBase = startSceneBase;
             Enum.TryParse(startScene.name, out SceneDefine define);
             CurrentSceneDefine = define;
             history.Add(CurrentSceneDefine);
+
+            currentSceneBase.Prepare(loader, this);
         }
 
         /// <summary>
@@ -37,7 +41,7 @@ namespace Project.System
         /// <param name="sceneDefine"></param>
         /// <param name="sceneData"></param>
         /// <param name="isResetHistory"></param>
-        public async UniTask Transition(SceneDefine sceneDefine, object sceneData, bool isResetHistory = false)
+        public async UniTask Transition(SceneDefine sceneDefine, object sceneData = null, bool isResetHistory = false)
         {
             // シーンをロード
             var result = await LoadScene(sceneDefine);
@@ -53,20 +57,21 @@ namespace Project.System
             }
             
             UnloadCurrentScene();
+            SceneManager.UnloadSceneAsync(currentScene);
 
             // シーン初期化
+            currentScene = result.scene;
             CurrentSceneDefine = sceneDefine;
             currentSceneBase = result.sceneBase;
             history.Add(sceneDefine);
-            await currentSceneBase.Prepare(loader, sceneData);
-            SceneManager.SetActiveScene(result.scene);
+            await currentSceneBase.Prepare(loader, this, sceneData);
         }
 
         /// <summary>
         /// 前のシーンに戻る
         /// </summary>
         /// <param name="sceneData"></param>
-        public async UniTask Back(object sceneData)
+        public async UniTask Back(object sceneData = null)
         {
             // 戻れるシーンを取得
             if (!HasBackScene)
@@ -85,13 +90,14 @@ namespace Project.System
             }
             
             UnloadCurrentScene();
+            SceneManager.UnloadSceneAsync(currentScene);
             
             // シーン初期化
+            currentScene = result.scene;
             CurrentSceneDefine = sceneDefine;
             currentSceneBase = result.sceneBase;
-            await currentSceneBase.Prepare(loader, sceneData);
+            await currentSceneBase.Prepare(loader, this, sceneData);
             await currentSceneBase.BackFromNext();  // 前のシーンに戻る際の処理
-            SceneManager.SetActiveScene(result.scene);
         }
 
         private async UniTask<(bool isSucceeded, Scene scene, SceneBase sceneBase)> LoadScene(SceneDefine sceneDefine)
@@ -99,6 +105,7 @@ namespace Project.System
             // 次のシーンをロード
             string scenePath = GetScenePath(sceneDefine);
             Scene scene = await loader.LoadSceneAsync(scenePath);
+            
             GameObject sceneObject = scene.GetRootGameObjects().FirstOrDefault(rootObject => rootObject.name == "Scene");    // SceneBaseがアタッチしているオブジェクトの名前は必ず「Scene」になる
             if (sceneObject == null)
             {
